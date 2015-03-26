bf092df97acf925488aa7cee0354e995570d3636
tomener <863758705@qq.com>
2015-03-26 14:28:15 +0800

    edit index.php

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * @version 1.0
+ * @author tomener
+ * @package Index
+ * @since 2015-03-25
+ * @license New BSD License.{@link http://www.opensource.org/licenses/bsd-license.php}
+ * @link http://blog.sina.com.cn/tomener
+ * @copyright Copyright (c) 2015 Tomener Software Studio
+ */
+
 define('ROOT_PATH', __DIR__.'/');
 
 echo ROOT_PATH;
